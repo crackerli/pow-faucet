0933edd99fe8252e434454f232c4809f360f4579
crackerli <crackerli@vmi434994.contaboserver.net>
2020-08-21 17:05:58 +0800

    Updates

--- precache-manifest.32a1103f0b4f3c9fdf9073570f7bf7bb.js
+++ precache-manifest.32a1103f0b4f3c9fdf9073570f7bf7bb.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "afd8bb8520d045a0aded1fe4498fd134",
+    "revision": "cf9ecacd444a22b6b5775d0982350ba5",
     "url": "/pow-faucet/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pow-faucet/static/js/2.6c37f5e9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7d3112bf921128757325",
-    "url": "/pow-faucet/static/js/main.5c6050bf.chunk.js"
+    "revision": "0f3878fbd57824ffd565",
+    "url": "/pow-faucet/static/js/main.40cf4b30.chunk.js"
   },
   {
     "revision": "ef087674465f2c485291",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pow-faucet/precache-manifest.a1062ecde01d12d88eacef9ffe8da3f2.js"
+  "/pow-faucet/precache-manifest.32a1103f0b4f3c9fdf9073570f7bf7bb.js"
 );
 
 self.addEventListener('message', (event) => {
